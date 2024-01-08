8f54ccbf99e33687da54645cef097f1dbdf1c2c0
Louis Asch <asch@entrecode.de>
2024-01-08 18:51:27 +0100

    adds basic stuff

--- event-fetching-prototype/event-fetching.js
+++ event-fetching-prototype/event-fetching.js
@@ -1,7 +1,17 @@
-const fs = require('fs').promises;  // Using promises version for fs
+const fs = require('fs').promises; // Using promises version for fs
 const Particle = require('particle-api-js');
 const particle = new Particle();
 let token;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+var cors = require('cors');
+app.use(cors());
+const PORT = 3001;
+
+app.use(bodyParser.json());
 
 // Wrap your code in an async function
 async function loginAndListDevices() {
@@ -17,12 +27,44 @@ async function loginAndListDevices() {
     // Use the obtained token to list devices
     const devices = await particle.listDevices({ auth: token });
 
+    let cedricAtHome = false;
+    let maxAtHome = false;
+    let LouisAtHome = false;
+
     //Get events filtered by name
-    particle.getEventStream({ deviceId: 'mine', auth: token }).then(function(stream) {
-        stream.on('event', function(data) {
-          console.log("Event: ", data);
-        });
+    particle.getEventStream({ name: 'Came home', auth: token }).then(function (stream) {
+      stream.on('event', function (data) {
+        if (data.data === 'Cedric') {
+          cedricAtHome = true;
+        } else if (data.data === 'Max') {
+          maxAtHome = true;
+        } else if (data.data === 'Louis') {
+          LouisAtHome = true;
+        }
+        console.log('Event: ', data);
+        console.log(cedricAtHome);
       });
+    });
+
+    // leaving event stream:
+    particle.getEventStream({ name: 'Left home', auth: token }).then(function (stream) {
+      stream.on('event', function (data) {
+        if (data.data === 'Cedric') {
+          cedricAtHome = false;
+        } else if (data.data === 'Max') {
+          maxAtHome = false;
+        } else if (data.data === 'Louis') {
+          LouisAtHome = false;
+        }
+        console.log('Event: ', data);
+        console.log(cedricAtHome);
+      });
+    });
+
+    app.get('/api/whoIsAtHome', (req, res) => {
+      res.send({ cedricAtHome, maxAtHome, LouisAtHome });
+    });
+
 
     console.log('Devices: ', devices);
   } catch (err) {
@@ -32,3 +74,7 @@ async function loginAndListDevices() {
 
 // Call the async function
 loginAndListDevices();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- web-app/src/components/MainPage.tsx
+++ web-app/src/components/MainPage.tsx
@@ -1,14 +1,47 @@
 // src/components/MainPage.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Header, Main, Footer, Navbar } from './StyledComponents';
 import RoundedProfileCard from './contentbox';
+import axios from 'axios';
 
-const handleOnline = () => {
-  return true
-}
+const handleOnlineCedric = () => {
+  axios.get('http://localhost:3001/api/whoIsAtHome').then((response) => {
+    return response.data.cedricAtHome;
+  });
+};
 
+const handleOnlineLouis = () => {
+  axios.get('http://localhost:3001/api/whoIsAtHome').then((response) => {
+    return response.data.louisAtHome;
+  });
+};
+
+const handleOnlineMax = () => {
+  axios.get('http://localhost:3001/api/whoIsAtHome').then((response) => {
+    return response.data.maxAtHome;
+  });
+};
 
 const MainPage: React.FC = () => {
+  const [atHomeC, setAtHomeC] = useState([]);
+  const [atHomeL, setAtHomeL] = useState([]);
+  const [atHomeM, setAtHomeM] = useState([]);
+
+  useEffect(() => {
+    // Fetch all items
+    axios
+      .get('http://localhost:3001/api/whoIsAtHome')
+      .then((response) => {
+        // Set state with result
+        setAtHomeC(response.data.cedricAtHome);
+        setAtHomeL(response.data.louisAtHome);
+        setAtHomeM(response.data.maxAtHome);
+      })
+      .catch((error) => console.error('Error fetching items:', error));
+  }, []);
+
+
+
   return (
     <Container>
       <Header>
@@ -22,7 +55,14 @@ const MainPage: React.FC = () => {
         </ul>
       </Navbar>
       <Main>
-        <RoundedProfileCard title={'Esslingen IOT WG'} profiles={[{ name: 'Louis', online: handleOnline() },{ name: 'Cedric', online: handleOnline() },{ name: 'Max', online: handleOnline() }]} />
+        <RoundedProfileCard
+          title={'Esslingen IOT WG'}
+          profiles={[
+            { name: 'Louis', online: atHomeL[0] },
+            { name: 'Cedric', online: atHomeC[0] },
+            { name: 'Max', online: atHomeM[0] },
+          ]}
+        />
       </Main>
       <Footer>
         <p>© 2024 PractiKey</p>
